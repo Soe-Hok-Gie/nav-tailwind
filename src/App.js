e3fee43a2cbd61386ed8d07e7f55751650694f5f
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-07-19 23:17:12 +0700

    menambahkan text slate-500

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-lg font-bold underline text-slate-500">Hello world!</h1>;
 };
 
 export default App;
