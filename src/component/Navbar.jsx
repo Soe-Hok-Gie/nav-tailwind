da8cd43280a52022d4cc9e514f5b1ce50f93e651
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-07-20 14:52:53 +0700

    membuat element sebuah kiri (logo)

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,7 +4,13 @@ const Navbar = () => {
   return (
     <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
       <div className="container">
-        <div className="flex items-center justify-between relative"></div>
+        <div className="flex items-center justify-between relative">
+          <div className="px-4">
+            <a href="#home" className="font-bold text-lg text-slate-500 block py-6">
+              CALIBURN
+            </a>
+          </div>
+        </div>
       </div>
     </header>
   );
