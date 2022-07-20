7ff669ef37714955959ab66559e58d2485a91dc8
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-07-20 14:45:32 +0700

    membuat sebuah container dan membagi menjadi dua buah sisi dgn properti justify-beetween

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Navbar = () => {
-  return <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10"></header>;
+  return (
+    <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+      <div className="container">
+        <div className="flex items-center justify-between"></div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
