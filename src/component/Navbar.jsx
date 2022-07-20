4ec55a623fb4c2b8d166ecf687eacc275a922f8b
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-07-20 14:42:29 +0700

    menambahkan z-index dan items-center

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <header className="bg-transparent absolute top-0 left-0 w-full flex"></header>;
+  return <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10"></header>;
 };
 
 export default Navbar;
