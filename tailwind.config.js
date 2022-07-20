ebe6a142d22981bc37fa31513efb9a5c99b68f4d
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-07-20 15:25:58 +0700

    membuat custom warna

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+      },
+    },
   },
   plugins: [],
 };
