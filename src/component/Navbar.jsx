92d45037f407dd6ad45dbaac9713f53be296eb61
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-07-21 14:33:10 +0700

    memnuat file javasscript utk logic togle

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
       <div className="container">
         <div className="flex items-center justify-between relative">
           <div className="px-4">
-            <a href="#home" className="font-bold text-lg text-slate-500 block py-6">
+            <a href="#home" className="font-bold text-lg text-primary block py-6">
               CALIBURN
             </a>
           </div>
           <div className="flex items-center px-4">
             <button id="humberger" name="humberger" type="button" className="block absolute right-4">
-              <span></span>
-              <span></span>
-              <span></span>
+              <span className="humberger-line"></span>
+              <span className="humberger-line"></span>
+              <span className="humberger-line"></span>
             </button>
           </div>
         </div>
